4a38f35e2a50b5dc2b3eb0f8f1f3a9e52504a31f
anycodef <anycodefast1@gmail.com>
2023-07-02 06:26:32 -0500

    Backup

--- code/states/Game/Level1/Resources/weapon.py
+++ code/states/Game/Level1/Resources/weapon.py
@@ -5,7 +5,9 @@ from pygame import KEYDOWN, KEYUP, K_SPACE
 
 from code.globals.Effects.arcVoltaic import ArcVoltaic, Point, CoordSys
 
-from pygame.mouse import get_pos
+from pygame.mouse import get_pos, get_pressed
+
+from random import choice
 
 
 class AbstractArcVoltaicWeaponCollision:
@@ -13,7 +15,7 @@ class AbstractArcVoltaicWeaponCollision:
 
 
 class AbstractMechanicsGui:
-    x, y = 0, 0
+    x, y = None, None
 
 
 class AbstractFacturesWeapon:
@@ -31,8 +33,21 @@ class ArcVoltaicWeaponMechanics(AbstractArcVoltaicWeaponCollision, AbstractMecha
     def __init__(self):
         pass
 
-    def run(self):
-        pass
+    @staticmethod
+    def set_goal_mouse():
+        if get_pressed()[0]:
+            AbstractMechanicsGui.x, AbstractMechanicsGui.y = get_pos()
+        else:
+            AbstractMechanicsGui.x, AbstractMechanicsGui.y = None, None
+
+    @staticmethod
+    def set_random_goal():
+        if AbstractArcVoltaicWeaponCollision.list_rect_for_collision:
+            goal = choice(AbstractArcVoltaicWeaponCollision.list_rect_for_collision)
+            AbstractMechanicsGui.x, AbstractMechanicsGui.y = goal
+            AbstractArcVoltaicWeaponCollision.list_rect_for_collision.remove(goal)
+        else:
+            AbstractMechanicsGui.x, AbstractMechanicsGui.y = None, None
 
 
 class ArcVoltaicWeaponGui(AbstractMechanicsGui, AbstractFacturesWeapon):
@@ -41,10 +56,12 @@ class ArcVoltaicWeaponGui(AbstractMechanicsGui, AbstractFacturesWeapon):
         self.__sysCord = CoordSys(screen.get_width(), screen.get_height())
 
     def show(self):
-        self.__arc_voltaic.show(10, Point(*self.__sysCord.coord_pygame_to_coord_system(
-            AbstractFacturesWeapon.x + AbstractFacturesWeapon.X_SHOOT,
-            AbstractFacturesWeapon.y + AbstractFacturesWeapon.Y_SHOOT)),
-                                Point(*self.__sysCord.coord_pygame_to_coord_system(*get_pos())))
+        if AbstractMechanicsGui.x and AbstractMechanicsGui.y:
+            self.__arc_voltaic.show(10, Point(*self.__sysCord.coord_pygame_to_coord_system(
+                AbstractFacturesWeapon.x + AbstractFacturesWeapon.X_SHOOT,
+                AbstractFacturesWeapon.y + AbstractFacturesWeapon.Y_SHOOT)),
+                                    Point(*self.__sysCord.coord_pygame_to_coord_system(
+                                        AbstractMechanicsGui.x, AbstractMechanicsGui.y)))
 
 
 class ArcVoltaicWeapon:
@@ -52,13 +69,14 @@ class ArcVoltaicWeapon:
         self.__mechanics = ArcVoltaicWeaponMechanics()
         self.__gui = ArcVoltaicWeaponGui(screen)
 
+    def set_goal_mouse(self):
+        self.__mechanics.set_goal_mouse()
+
+    def set_random_goal(self):
+        self.__mechanics.set_random_goal()
+
     def run(self):
         self.__gui.show()
-
-
-def update_position(x_character, y_character, current_direction):
-    AbstractFacturesWeapon.x = x_character + AbstractFacturesWeapon.X_RELATIVE[current_direction]
-    AbstractFacturesWeapon.y = y_character + AbstractFacturesWeapon.Y_RELATIVE
 
 
 class Weapon:
@@ -72,6 +90,7 @@ class Weapon:
         self.__x_shooting = 5
 
         self.__shooting = False
+        self.__auto_shooting = False
 
         # load default images
         self.load_weapon()
@@ -85,13 +104,25 @@ class Weapon:
             self.__img.__setitem__(direction, load(join(path_root_project, 'src', 'weapon', f'{name}-{direction}.png')))
 
     def manage_events(self, queue_event):
+        if not self.__auto_shooting:
+            if get_pressed()[0]:
+                self.__arc.set_goal_mouse()
+                self.__shooting = True
+            else:
+                self.__shooting = False
+
         for event in queue_event:
             if event.type == KEYDOWN:
                 if event.key == K_SPACE:
                     self.__shooting = not self.__shooting
+                    self.__auto_shooting = True
             if event.type == KEYUP:
                 if event.key == K_SPACE:
                     self.__shooting = not self.__shooting
+                    self.__auto_shooting = False
+
+        if self.__auto_shooting:
+            self.__arc.set_random_goal()
 
     def __shoot(self):
         if self.__shooting:
@@ -106,8 +137,13 @@ class Weapon:
         self.__screen.blit(self.__img[current_direction], (AbstractFacturesWeapon.x + self.__x_shoot,
                                                            AbstractFacturesWeapon.y))
 
+    @staticmethod
+    def update_position(x_character, y_character, current_direction):
+        AbstractFacturesWeapon.x = x_character + AbstractFacturesWeapon.X_RELATIVE[current_direction]
+        AbstractFacturesWeapon.y = y_character + AbstractFacturesWeapon.Y_RELATIVE
+
     def run(self, x_character, y_character, current_direction):
-        update_position(x_character, y_character, current_direction)
+        self.update_position(x_character, y_character, current_direction)
         self.__shoot()
         self.__show(current_direction)
 
